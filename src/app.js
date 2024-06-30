00a5c8cf48d6ee50b9b17c60cf883b4b1e3bfc73
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-06-30 07:45:54 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+// sets up a middleware to parse incoming JSON payloads in HTTP requests. The { limit: "5mb" } 
+//option specifies that the size limit for incoming JSON payloads is 5 megabytes. 
+//If a request body exceeds this limit, an error will be thrown.
+app.use(express.json({ limit: "5mb" })); 
+app.use(morgan("dev"));
+
+app.disable("x-powered-by"); // to disable express server fingerprinting
+
+const indexRoutes = require("./routes");
+
+app.use("/api/v1", indexRoutes);
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,10 @@
+const app = require("/app");
+const logger = require("/utils/logger");
+const config = require("../utils/config");
+const connectMongoDB = require("./db/mongodb");
+
+
+app.listen(config.PORT, () => {
+    connectMongoDB();
+    logger.info(`Server is ready and running on port: ${config.PORT}`);
+})
